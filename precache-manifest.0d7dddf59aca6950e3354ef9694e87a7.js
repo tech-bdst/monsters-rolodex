6ffa7f70a0c4643cb153be22be648bbe49d9220d
tech-bdst <60012193+tech-bdst@users.noreply.github.com>
2020-04-10 18:16:16 -0400

    Updates

---
+++ precache-manifest.0d7dddf59aca6950e3354ef9694e87a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7ef8ac65da73dd87b4f631873ca8dbf",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "61d6aabf0a1c2f10a99c",
+    "url": "/monsters-rolodex/static/css/main.21279b08.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61d6aabf0a1c2f10a99c",
+    "url": "/monsters-rolodex/static/js/main.534aa21d.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
